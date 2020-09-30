2504876b3b83a180fca3fc16cac0cb06d9d1ec23
JerWill <jeremie.willemin@gmail.com>
2020-09-30 23:02:31 +0200

    setuptool package name changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytrask-Nimelli", # Replace with your own username
+    name="pytrask", # Replace with your own username
     version="0.0.1",
     author="Nimelli",
     author_email="",
