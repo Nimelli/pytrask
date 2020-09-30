9f585dd5ca43468c06b0ec781d241c44faf6b42a
JerWill <jeremie.willemin@gmail.com>
2020-09-30 22:40:15 +0200

    default start to current directory in recursive mode, ui doubleclick correction

--- pytrask/__main__.py
+++ pytrask/__main__.py
@@ -2,7 +2,7 @@
 # python -m pytrask
 # execute the following, ie execute pytraskgui
 
-from src.pytraskgui import *
+from pytrask.src.pytraskgui import *
 
 # same as in pytraskgui.py
 import argparse
@@ -12,7 +12,7 @@ parser.add_argument('-e', '--embedded', action='store_true', help="shows applica
 parser.add_argument('-t', '--text-editor', dest='editor', help="Choose which editor will open your trasks: {}. Default is vscode".format(list(supported_editor.keys())))
 parser.add_argument('-f', '--files', dest='files', nargs='+', help="Add path of files to analyse")
 parser.add_argument('-d', '--dir', dest='directory', default='.', help="Add path of directory to analyse")
-parser.add_argument('-r', '--recursive', action='store_true', help="Make directory (-d) analisys recursive")
+parser.add_argument('-nr', '--non-recursive', dest='nonrecursive', action='store_true', help="Make directory (-d) analisys non-recursive, recursive by default")
 
 args = parser.parse_args()
 
@@ -21,7 +21,8 @@ if(args.files != None):
         trasker.register_file(file)
 
 if(args.directory != None):
-    trasker.register_directory(args.directory, recursive=args.recursive)
+    recursive = not args.nonrecursive
+    trasker.register_directory(args.directory, recursive=recursive)
 
 if(args.embedded):
     main(True, args.editor)
--- pytrask/src/pytraskgui.py
+++ pytrask/src/pytraskgui.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--text-editor', dest='editor', help="Choose which editor will open your trasks: {}. Default is vscode".format(list(supported_editor.keys())))
     parser.add_argument('-f', '--files', dest='files', nargs='+', help="Add path of files to analyse")
     parser.add_argument('-d', '--dir', dest='directory', default='.', help="Add path of directory to analyse")
-    parser.add_argument('-r', '--recursive', action='store_true', help="Make directory (-d) analisys recursive")
+    parser.add_argument('-nr', '--non-recursive', dest='nonrecursive', action='store_true', help="Make directory (-d) analisys non-recursive, recursive by default")
 
     args = parser.parse_args()
 
@@ -150,7 +150,8 @@ if __name__ == "__main__":
             trasker.register_file(file)
 
     if(args.directory != None):
-        trasker.register_directory(args.directory, recursive=args.recursive)
+        recursive = not args.nonrecursive
+        trasker.register_directory(args.directory, recursive=recursive)
 
     if(args.embedded):
         main(True, args.editor)
