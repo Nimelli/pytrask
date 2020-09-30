717ddf47af2a9069bb2820f1c1c3c9750d70e08e
JerWill <jeremie.willemin@gmail.com>
2020-09-30 20:43:37 +0200

    structure change for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Nimelli",
+    author_email="",
+    description="Minimalist Kanban task management tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nimelli/pytrask",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
